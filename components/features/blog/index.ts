f43e7c1d2265df8805f31300d1ffeae384aa3d62
William Callahan <william@aventure.vc>
2025-03-25 00:51:46 -0700

    Fix: Misc small bugs

--- components/features/blog/index.ts
+++ components/features/blog/index.ts
@@ -1,9 +1,10 @@
 /**
  * Blog Feature Components
- * 
+ *
  * Central export point for all blog-related components
  */
 
-export { BlogArticle } from './blog-article';
+export { BlogWrapper } from './blog-article/blog-wrapper';
+export { BlogArticle } from './blog-article/blog-article';
 export { BlogList } from './blog-list';
 export { BlogAuthor, BlogTags } from './shared';
