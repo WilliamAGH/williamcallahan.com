8de159040a5c80e6964e7ed83121d10954d6d9ab
William Callahan <william@aventure.vc>
2025-04-09 16:52:07 -0700

    Improve config and file names for better client/server separation and consistent file naming

--- components/features/education/education-card.client.tsx
+++ components/features/education/education-card.client.tsx
@@ -20,7 +20,7 @@
 
 import { ExternalLink as ExternalLinkIcon } from 'lucide-react';
 import { LogoImage } from '../../../components/ui';
-import { ExternalLink } from '../../ui/externalLink';
+import { ExternalLink } from '../../ui/external-link.client';
 import type { Education } from '../../../types/education';
 
 /**
