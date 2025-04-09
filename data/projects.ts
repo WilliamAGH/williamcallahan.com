1eb903d1c391996ebf86edfbc6e96ab7b377918c
William Callahan <william@aventure.vc>
2025-04-09 14:54:40 -0700

    Update text

--- data/projects.ts
+++ data/projects.ts
@@ -3,8 +3,8 @@ import type { Project } from '@/types/project';
 export const projects: Project[] = [
   {
     name: 'williamcallahan.com',
-    description: 'My personal site featuring an interactive terminal, dynamic OG images, API for fetching of logo images for investments (with refetch/cache logic), MDX blog posts, and more. Built with Next.js App Router, TypeScript, Tailwind CSS, and deployable in any Docker hosted container, tested across cloud environments (e.g., Vercel, GCP, Oracle Cloud, Hetzner, Railway, Fly.io, and on personal servers).',
-    shortSummary: 'Interactive terminal site with dynamic OG images',
+    description: 'This personal site featuring interactive macOS-style window components (Terminal, Code Blocks, Images) with close/minimize/maximize controls and animations. Includes a lot of dynamic content, an API for fetching investment logos (with refetch/cache logic), MDX blog posts, and more. Built with Next.js App Router, TypeScript, Tailwind CSS, and deployable via Docker across various cloud environments (Vercel, GCP, Oracle Cloud, Hetzner, Railway, Fly.io, etc.).',
+    shortSummary: 'Interactive personal site with beautiful terminal/code components & other dynamic content',
     url: 'https://williamcallahan.com',
     image: '/images/williamcallahan-com-project.png',
     tags: ['Next.js', 'TypeScript', 'Tailwind CSS', 'React',  'MDX', 'Server Components'],
