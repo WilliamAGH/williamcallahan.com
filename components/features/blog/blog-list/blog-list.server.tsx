51b929654a94b00eef860f3c84bed950293ea52e
William Callahan <william@aventure.vc>
2025-04-09 17:47:08 -0700

    Fix blog home

--- components/features/blog/blog-list/blog-list.server.tsx
+++ components/features/blog/blog-list/blog-list.server.tsx
@@ -34,7 +34,7 @@ interface BlogListServerProps {
 export async function BlogListServer({ posts }: BlogListServerProps): Promise<JSX.Element> {
   return (
     <div className="space-y-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {posts.map((post) => (
           <BlogCard key={post.slug} post={post} />
         ))}
