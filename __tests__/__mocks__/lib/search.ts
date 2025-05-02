b52a12d1c59c4b80fb6aa06cc1875f8c1a101d5b
William Callahan <william@aventure.vc>
2025-05-02 13:35:43 -0700

    tests: improve test coverage

---
+++ __tests__/__mocks__/lib/search.ts
@@ -0,0 +1,8 @@
+/**
+ * Mock search module for tests
+ */
+
+export const searchExperience = jest.fn().mockResolvedValue([]);
+export const searchEducation = jest.fn().mockResolvedValue([]);
+export const searchInvestments = jest.fn().mockResolvedValue([]);
+export const searchBookmarks = jest.fn().mockResolvedValue([]);
