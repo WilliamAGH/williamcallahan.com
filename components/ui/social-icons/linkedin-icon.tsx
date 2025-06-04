cc6342826e4a8aa1c4c3e042fce621bef8110179
William Callahan <william@aventure.vc>
2025-06-03 22:42:37 -0700

    feat(icons): add title to LinkedIn icon

--- components/ui/social-icons/linkedin-icon.tsx
+++ components/ui/social-icons/linkedin-icon.tsx
@@ -21,6 +21,7 @@ export const LinkedIn = forwardRef<SVGSVGElement, LucideProps>(function LinkedIn
       viewBox="0 0 24 24"
       data-transform-fix="true"
     >
+      <title>LinkedIn</title>
       <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2a2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
       <rect x="2" y="9" width="4" height="12" />
       <circle cx="4" cy="4" r="2" />
