c0d644bd863705df8f5c4eec446bbfa52764617a
William Callahan <william@aventure.vc>
2025-06-16 23:27:36 -0700

    refactor(data): standardize import statements and string quotes across data files

--- data/blog/posts.ts
+++ data/blog/posts.ts
@@ -2,7 +2,7 @@
  * Blog Posts Data
  */
 
-import type { BlogPost } from '@/types/blog';
+import type { BlogPost } from "@/types/blog";
 
 // Empty array since we're moving to MDX files
 export const posts: BlogPost[] = [];
